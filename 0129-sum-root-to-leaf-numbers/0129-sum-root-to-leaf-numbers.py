d46ed1d8dfce091b0cbbdec5f0ca8c1bdde3fe7a
Shambhavi Pandey <112650493+ShambhaviPandey2021@users.noreply.github.com>
2024-04-16 01:03:09 +0530

    Time: 33 ms (74.15%), Space: 16.4 MB (81.00%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -10,7 +10,7 @@ class Solution:
             if not node:
                 return 0
             path_sum = path_sum * 10 + node.val
-            if not node.left and not node.right:  # If it's a leaf node
+            if not node.left and not node.right:
                 return path_sum
             return dfs(node.left, path_sum) + dfs(node.right, path_sum)
         
