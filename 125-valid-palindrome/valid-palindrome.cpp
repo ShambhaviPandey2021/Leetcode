a0c153cc11fdbb591fe1ca0d30ec4167f445af88
Shambhavi Pandey <112650493+ShambhaviPandey2021@users.noreply.github.com>
2024-07-10 23:23:18 +0530

    Time: 6 ms (45.78%) | Memory: 9 MB (28.58%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    bool isPalindrome(std::string s) {
+   
+        std::string filtered;
+        for (char c : s) {
+            if (std::isalnum(c)) {
+                filtered += std::tolower(c);
+            }
+        }
+
+        int start = 0;
+        int end = filtered.length() - 1;
+        
+        return helper(filtered, start, end);
+    }
+
+private:
+    bool helper(const std::string& s, int start, int end) {
+        while (start < end) {
+            if (s[start] != s[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
